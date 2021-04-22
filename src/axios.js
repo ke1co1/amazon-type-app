ef8a5151c4b18aab17d5cd2d9767434f20c71bc6
Kelvin Collado <kelvin@liniax.com>
2021-04-22 19:53:33 -0400

    updated baseURL for axios instance from localhost to firebase hosted

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const instance = axios.create({
   //the API (cloud function) URL
-  baseURL: "http://localhost:5001/clone-2ff04/us-central1/api",
+  // baseURL: "http://localhost:5001/clone-2ff04/us-central1/api",
+  baseURL: "https://us-central1-clone-2ff04.cloudfunctions.net/api",
 });
 
 export default instance;
